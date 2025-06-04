d2aa10968a30c9c97ce18fee912c52cf7292c657
yuanguandong <yuangd@gantsoftware.com>
2025-06-04 09:44:40 +0800

    feat: add vue demo

---
+++ demos/vue/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="player-wrapper">
+    <HelloWorld src="Data Server Center.iplayer" />
+  </div>
+</template>
+
+<style>
+.player-wrapper {
+  width: 100vw;
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+  top: 0;
+  left: 0;
+  position: fixed;
+}
+</style>
---
+++ demos/vue/src/components/HelloWorld.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { onMounted, ref } from 'vue'
+import ICraftPlayer from '@icraft/player'
+
+const props = defineProps({
+  src: {
+    type: String,
+    required: true,
+  },
+})
+
+const containerRef = ref(null)
+let player = null
+
+onMounted(() => {
+  if (containerRef.value) {
+    player = new ICraftPlayer({
+      src: props.src,
+      container: containerRef.value,
+    })
+  }
+})
+</script>
+
+<template>
+  <div ref="containerRef" class="player-content"></div>
+</template>
+
+<style scoped>
+.player-content {
+  width: 100%;
+  height: 100%;
+}
+</style>
